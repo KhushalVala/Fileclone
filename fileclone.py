545cf13c691eaf4ab1fe00d55088fc7376e995d5
Vala Khushal <76585038+KhushalVala@users.noreply.github.com>
2023-03-07 14:13:28 +0530

    Update fileclone.py

--- fileclone.py
+++ fileclone.py
@@ -1,1 +1,16 @@
-import Fileclone
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from Fileclone32 import Legend
+    Legend()
+elif bit == '32bit':
+    from Fileclone import Legend
+    Legend()
+else:
+    print('\n YOUR DEVICE IS NOT SUPPORT THIS COMMAND')
+    os.system('exit')
